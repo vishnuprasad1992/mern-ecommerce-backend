96a8d29ce40c8cb2d3bf918ed00c8c885deb33be
vishnuprasad1992 <75542863+vishnuprasad1992@users.noreply.github.com>
2021-07-11 11:13:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ app.use("/api/products",productRoute);
 app.use("/api/user",userRoute)
 app.use("/api/orders",orderRoute)
 
-if(process.env.NODE_ENV ==="production"){
-    app.use("/",express.static("../client/build"))
+// if(process.env.NODE_ENV ==="production"){
+//     app.use("/",express.static("../client/build"))
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'..client/build/index.html'))
-    })
-}
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'..client/build/index.html'))
+//     })
+// }
 
 app.listen(port,()=>{
     console.log(`server running on http://localhost:${port}`);
